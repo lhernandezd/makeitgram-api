296795c74c1b5743d1f6721eebd2ce357b40688b
Luis Hernández <ldhdv95@gmail.com>
2018-08-10 15:36:35 -0500

    Add User Model and Relationship with Post Model

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_09_013430) do
+ActiveRecord::Schema.define(version: 2018_08_10_000830) do
 
   create_table "posts", force: :cascade do |t|
     t.string "description"
     t.string "location"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "firstname"
+    t.string "lastname"
+    t.string "username"
+    t.string "email"
+    t.string "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
